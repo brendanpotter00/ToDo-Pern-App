ed97d1b1d3329d83cacc2445ec015fdd830c67bc
Brendan Potter <brendanpotter00@gmail.com>
2021-11-13 00:48:57 -0600

    set up the database and doing routes now

--- server/index.js
+++ server/index.js
@@ -1,20 +1,50 @@
-//step 1
+//step 1 REQUIREMENTS/IMPORTS
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
-//middleware
+//MIDDLEWARE-----------------------
 app.use(cors());
 
 //getting data from the client side
 //from the request.body obj
-app.use(express.json());
+app.use(express.json()); //req.body
+
+//ROUTES----------
+//create a todo
+
+app.post("/todos", (req, res) => {
+    //await = wait for a function to execute 
+    try {
+
+        
+
+
+    } catch (err){
+        console.error(err.message);
+    }
+    
+})
+
+//get all todos
+
+
+//get a todo
+
+
+//update a todo 
+
+
+//delete a todo
 
 
 
+
+
+//SERVER------------------------------
 app.listen(5000, () => {
     console.log("server started and is on port 5000");
 });
 
-//next create a database and table in postgres
-//change
+
