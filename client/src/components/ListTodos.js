232a786075ae34c7f831b8e55e6ede6f0b8feca3
Brendan Potter <brendanpotter00@gmail.com>
2021-11-16 23:04:53 -0600

    working on the edit feature

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,19 @@ import React, { Fragment, useEffect, useState } from "react";
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  //delete functions from
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
@@ -42,7 +55,14 @@ const ListTodos = () => {
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
